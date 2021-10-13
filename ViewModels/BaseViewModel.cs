45df2669ca4a304b9a4fe835ed32dd6a85a184e3
Snehal Sapre <snehalsapre@Ashwins-MacBook-Air.local>
2021-10-13 20:30:46 +0530

    Datagrid solution

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace GitSpecificApp.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public string this[string columnName] => throw new NotImplementedException();
+
+        public string Error => throw new NotImplementedException();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
